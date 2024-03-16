a0f8904593efc84b84677ff090466e3325f6b7a4
Many <manuel.garcia4599@alumnos.udg.mx>
2024-03-16 11:26:30 -0600

    Final

---
+++ Sprite2D.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var velocidad = 300
+var anguloVelocidad = PI
+
+func _process(delta):
+	var direccion = 0
+	if Input.is_action_pressed("ui_left"):
+		direccion = -1
+	if Input.is_action_pressed("ui_right"):
+		direccion = 1
+
+	rotation += anguloVelocidad * direccion * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * velocidad
+
+	position += velocity * delta
+	
+	
